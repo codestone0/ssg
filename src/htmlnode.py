1b678bfb85253ce15688517da4aa8d927c90f463
Chuck Lee <chuck@climoa.org>
2026-01-15 01:35:24 -0500

    2.4 leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,16 +1,16 @@
 class HTMLNode:
     def __init__(self, 
-                 tag=None, 
-                 value=None, 
-                 children=None, 
-                 props=None):
+                tag=None, 
+                value=None, 
+                children=None, 
+                props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
     def to_html(self):
-        raise Exception(NotImplementedError)
+        raise NotImplementedError('to_html not implemented')
     
     def props_to_html(self):
         if not self.props:
@@ -21,5 +21,19 @@ class HTMLNode:
         return props_str
     
     def __repr__(self):
-        return f'tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}'
+        return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, props: {self.props})'
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+        if not self.value:
+            raise ValueError("value required for leaf nodes")
+        
+    def to_html(self):
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, props: {self.props})'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,13 +1,59 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
-        node = HTMLNode("<a>", "forgotten bones", [], {'href':'http://www.dec.com','target':'_self'})
-        self.assertEqual(node.props_to_html(), ' href="http://www.dec.com" target="_self"')
+        node = HTMLNode(
+            "a", 
+            "forgotten bones", 
+            [], 
+            {'href':'http://www.dec.com','target':'_self'}
+        )
+        self.assertEqual(
+            node.props_to_html(), 
+            ' href="http://www.dec.com" target="_self"'
+        )
 
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "the story isn't over yet"
+        )
+        self.assertEqual(node.tag, 'div')
+        self.assertEqual(node.value, "the story isn't over yet")
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "hope is...",
+            None,
+            {'class': 'alpha'}
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, hope is..., children: None, props: {'class': 'alpha'})"
+        )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Learn here!", {"href": "https://www.boot.dev"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.boot.dev">Learn here!</a>'
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Trogdor!!")
+        self.assertEqual(node.to_html(), "Trogdor!!")
         
 
 if __name__ == "__main__":
