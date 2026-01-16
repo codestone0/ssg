f7ac83a0cbae941e9d72bc3b2344c1a636915475
Chuck Lee <chuck@climoa.org>
2026-01-16 17:46:07 -0500

    3.4 extract links and images

--- src/test_md_parse.py
+++ src/test_md_parse.py
@@ -1,9 +1,9 @@
 import unittest
-from md_parse import split_nodes_delimiter
+from md_parse import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextType, TextNode
 
-class TestMd_Parse(unittest.TestCase):
-    def test_split_nodes_delimiter_code(self):
+class Test_split_nodes_delimiter(unittest.TestCase):
+    def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(
@@ -15,7 +15,7 @@ class TestMd_Parse(unittest.TestCase):
             new_nodes,
         )
 
-    def test_split_nodes_delimiter_bold(self):
+    def test_bold(self):
         node = TextNode("normal blah blah **bold words** blah word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertEqual(
@@ -27,7 +27,7 @@ class TestMd_Parse(unittest.TestCase):
             new_nodes,
         )
 
-    def test_split_nodes_delimiter_italic(self):
+    def test_italic(self):
         node = TextNode("more plain text _italicized words_ meh meh", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
         self.assertEqual(
@@ -39,6 +39,40 @@ class TestMd_Parse(unittest.TestCase):
             new_nodes,
         )
 
+class Test_extract_markdown_images(unittest.TestCase):
+    def test_single(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_double(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)" \
+        " and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        reference = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"), 
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ]
+        matches = extract_markdown_images(text)
+        self.assertListEqual(reference, matches)
+
+
+class Test_extract_markdown_links(unittest.TestCase):
+    def test_single(self):
+        matches = extract_markdown_links(
+            "Lorem ipsum dolor sit amet, consec[tetur](https://i.imgur.com/zjjcJKZ.png)adipiscing"
+        )
+        self.assertListEqual([("tetur", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_double(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev)" \
+        " and [to youtube](https://www.youtube.com/@bootdotdev)"
+        reference = [
+            ("to boot dev", "https://www.boot.dev"), 
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ]
+        matches = extract_markdown_links(text)
+        self.assertListEqual(reference, matches)
 
 if __name__ == "__main__":
     unittest.main()
