929d434e91f0072600976a4ea4cc2a83ecd9fdf1
Chuck Lee <chuck@climoa.org>
2026-01-17 01:53:34 -0500

    3.5 split_nodes_link & split_nodes_image

--- src/test_inlinemdparse.py
+++ src/test_inlinemdparse.py
@@ -1,5 +1,9 @@
 import unittest
-from md_parse import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inlinemdparse import (
+    split_nodes_delimiter, extract_markdown_images, 
+    extract_markdown_links, split_nodes_image,
+    split_nodes_link
+)
 from textnode import TextType, TextNode
 
 class Test_split_nodes_delimiter(unittest.TestCase):
@@ -35,6 +39,46 @@ class Test_split_nodes_delimiter(unittest.TestCase):
                 TextNode("more plain text ", TextType.TEXT),
                 TextNode("italicized words", TextType.ITALIC),
                 TextNode(" meh meh", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+class Test_split_nodes_image(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) " \
+            "and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+class Test_split_nodes_link(unittest.TestCase):
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [image](https://i.imgur.com/zjjcJKZ.png) " \
+            "and another [second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
             ],
             new_nodes,
         )
