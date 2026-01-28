adb82035a000792c89f3857db190051520e10dbf
Chuck Lee <chuck@climoa.org>
2026-01-28 02:00:53 -0500

    4.2 block_to_block_type

--- src/blockmdparse.py
+++ src/blockmdparse.py
@@ -1,6 +1,29 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     for i in range(len(blocks)):
         blocks[i] = blocks[i].strip()
     return [block for block in blocks if len(block)]
+
+def block_to_block_type(block):
+    if re.fullmatch(r"^#{1,6} [\w\d\s\.]+", block):
+        return BlockType.HEADING
+    if re.fullmatch(r"^```([\S]*)?\n[[\s\S]+\n]*\```$", block):
+        return BlockType.CODE
+    if re.fullmatch(r"((>)(.{1,}\n?))+", block):
+        return BlockType.QUOTE
+    if re.fullmatch(r"((-)( {1,})(.{1,}\n?))+", block):
+        return BlockType.UNORDERED_LIST
+    if re.fullmatch(r"(([0-9]{1,})\. (.{1,}\n?))+", block):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_blockmdparse.py
+++ src/test_blockmdparse.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blockmdparse import markdown_to_blocks
+from blockmdparse import markdown_to_blocks, BlockType, block_to_block_type
 
 class Test_markdown_to_blocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -25,3 +25,34 @@ This is the same paragraph on a new line
                 '- This is a list\n- with items'
             ]
         )
+
+class Test_block_to_block_type(unittest.TestCase):
+    def test_block_to_type(self):
+        p_block = "sd drfawsd fafasdf aergf asdc " \
+                  "asdfsdrtg asdvqwef asxzc asrg asdcv awergf " \
+                  "tyujn cjnuyb987y kjhn98awe fkjnikjunaewrf 8"
+        h1_block = "# Lookit that big ol heading."
+        h6_block = "###### So tiny"
+        code_block = "```\n" \
+                     "spaghetti code ( 5 ? 'heads' : 'tails' )\n" \
+                     "def pizza_me():\n" \
+                     "bool `javascript string`\n" \
+                     "```"
+        quote_block = "> There is no try\n" \
+                      ">only do."
+        ul_block = "- some stuff\n" \
+                   "- eggs\n" \
+                   "- donuts"
+        ol_block = "1. one, two\n" \
+                   "2. cows go moo\n" \
+                   "3. three, four\n" \
+                   "4. I lost the door"
+
+        self.assertEqual(block_to_block_type(p_block), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(h1_block), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(h6_block), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(code_block), BlockType.CODE)
+        self.assertEqual(block_to_block_type(quote_block), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(ul_block), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(ol_block), BlockType.ORDERED_LIST)
+        
